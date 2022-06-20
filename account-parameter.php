14cab3fb01144b4906153218771d49f55463d297
Hxmza49 <hamza.jaait@insa-cvl.fr>
2022-06-20 15:18:44 +0200

    backend account-parameters.php

--- account-parameter.php
+++ account-parameter.php
@@ -1,3 +1,18 @@
+<?php
+    logged_only();
+   
+    function logged_only(){
+        if(session_status() == PHP_SESSION_NONE){
+            session_start();
+        }
+        if(!isset($_SESSION['email'])){
+            header('Location: login-form.php');
+            exit();
+        }
+    }
+    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -25,14 +40,14 @@
                     <h4 class="text-right">Profile Settings</h4>
                 </div>
                 <div class="row mt-2">
-                    <div class="col-md-6"><label class="labels">Name</label><input type="text" class="form-control " placeholder="Amine" value="" name="first-name" disabled></div>
-                    <div class="col-md-6"><label class="labels">Last Name</label><input type="text" class="form-control" value="" placeholder="JAIIT" name="last-name" disabled></div>
+                    <div class="col-md-6"><label class="labels">Name</label><input type="text" class="form-control " placeholder="<?php echo($_SESSION['name']);?>" value="" name="first-name" disabled></div>
+                    <div class="col-md-6"><label class="labels">Last Name</label><input type="text" class="form-control" value="" placeholder="<?php echo($_SESSION['last name']);?>" name="last-name" disabled></div>
                 </div>
                 <div class="row mt-3">
-                    <div class="col-md-6"><label class="labels">Telephone Number</label><input type="text" class="form-control" placeholder="0600000000" value=""  name="telephone-number" disabled></div>
-                    <div class="col-md-6"><label class="labels">Student Card</label><input type="text" class="form-control" placeholder="02931xxxxx" value="" name="student-card" disabled></div>
-                    <div class="col-md-12"><label class="labels">Address </label><input type="text" class="form-control" placeholder="125 Rue charlet" value="" name="address" disabled></div>
-                    <div class="col-md-12"><label class="labels">Email ID</label><input type="text" class="form-control" placeholder="example@hotmail.com" value="" name="email" disabled></div>
+                    <div class="col-md-6"><label class="labels">Telephone Number</label><input type="text" class="form-control" placeholder=<?php echo($_SESSION['phone']);?> value=""  name="telephone-number" disabled></div>
+                    <div class="col-md-6"><label class="labels">Student Card</label><input type="text" class="form-control" placeholder=<?php echo($_SESSION['card number']);?> value="" name="student-card" disabled></div>
+                    <div class="col-md-12"><label class="labels">Address </label><input type="text" class="form-control" placeholder=<?php echo($_SESSION['address']);?> value="" name="address" disabled></div>
+                    <div class="col-md-12"><label class="labels">Email ID</label><input type="text" class="form-control" placeholder=<?php echo($_SESSION['email']);?> value="" name="email" disabled></div>
                     
                 </div>
                 
